752862928740c411755e518008a6fdb8e92de01f
Fredrik Bergenlid <fredrik.bergenlid@gmail.com>
2021-12-07 11:43:37 +0100

    x -> 0x

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -27,7 +27,15 @@ macro_rules! aoc {
     }
 }
 
-mod day1;
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
 mod day10;
 mod day11;
 mod day12;
@@ -38,31 +46,23 @@ mod day16;
 mod day17;
 mod day18;
 mod day19;
-mod day2;
 mod day20;
 mod day21;
 mod day22;
 mod day23;
 mod day24;
 mod day25;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
 
 aoc! {
-    1 => day1,
-    2 => day2,
-    3 => day3,
-    4 => day4,
-    5 => day5,
-    6 => day6,
-    7 => day7,
-    8 => day8,
-    9 => day9,
+    1 => day01,
+    2 => day02,
+    3 => day03,
+    4 => day04,
+    5 => day05,
+    6 => day06,
+    7 => day07,
+    8 => day08,
+    9 => day09,
     10 => day10,
     11 => day11,
     12 => day12,
@@ -73,10 +73,10 @@ aoc! {
     17 => day17,
     18 => day18,
     19 => day19,
-    20=> day20,
-    21=> day21,
-    22=> day22,
-    23=> day23,
-    24=> day24,
-    25=> day25
+    20 => day20,
+    21 => day21,
+    22 => day22,
+    23 => day23,
+    24 => day24,
+    25 => day25
 }
