b88d7ba5c9ef0b0d47f91fa8cce71b36168840db
Fredrik Bergenlid <fredrik.bergenlid@gmail.com>
2021-12-17 19:07:02 +0100

    day 17

--- 2021/src/day17.rs
+++ 2021/src/day17.rs
@@ -1,25 +1,102 @@
-pub fn solve_part_1(_input_str: &str) -> usize {
-    0
+use recap::Recap;
+use serde::Deserialize;
+use std::cmp::{max, min};
+
+#[derive(Debug, Deserialize, Recap)]
+#[recap(
+    regex = r"^target area: x=(?P<x0>\d+)\.\.(?P<x1>\d+), y=(?P<y0>-?\d+)\.\.(?P<y1>-?\d+)\n?$"
+)]
+struct TargetArea {
+    x0: i32,
+    x1: i32,
+    y0: i32,
+    y1: i32,
 }
-pub fn solve_part_2(_input_str: &str) -> usize {
-    0
+
+pub fn solve_part_1(input_str: &str) -> i32 {
+    let ta = input_str.parse::<TargetArea>().unwrap();
+    ta.y0 * (ta.y0 + 1) / 2
+}
+
+// Return the number of steps (iterations) (min, max) when the initial x velocity hits the target
+fn hit_steps_x(xv: i32, t: &TargetArea) -> Option<(i32, i32)> {
+    if xv > t.x1 || xv < 1 {
+        return None;
+    }
+
+    let x_coef = (2 * xv + 1) as f64 / 2 as f64;
+    let min = (x_coef - (x_coef.powf(2.0) - t.x0 as f64 * 2.0).sqrt()).ceil() as i32;
+    let max = (x_coef - (x_coef.powf(2.0) - t.x1 as f64 * 2.0).sqrt()).floor() as i32;
+
+    if min > 0 && max == 0 {
+        Some((min, i32::MAX))
+    } else {
+        Some((min, max))
+    }
+}
+
+fn hit_steps_y(yv: i32, t: &TargetArea) -> Option<(i32, i32)> {
+    if yv < t.y0 || yv >= t.y0.abs() {
+        return None;
+    }
+
+    let y_coef = (2 * yv + 1) as f64 / 2 as f64;
+    let min = (y_coef + (y_coef.powf(2.0) - t.y1 as f64 * 2.0).sqrt()).ceil() as i32;
+    let max = (y_coef + (y_coef.powf(2.0) - t.y0 as f64 * 2.0).sqrt()).floor() as i32;
+
+    if min == 0 || max == 0 || min > max {
+        None
+    } else {
+        Some((min, max))
+    }
+}
+
+pub fn solve_part_2(input_str: &str) -> usize {
+    let t = input_str.parse::<TargetArea>().unwrap();
+    let xv_min = (((t.x0 * 8 + 1) as f64).sqrt() / 2.0 - 0.5).ceil() as i32;
+    let yv_max = (t.y0 + 1).abs();
+
+    let mut xvs = vec![];
+    for i in xv_min..=t.x1 {
+        if let Some((start, end)) = hit_steps_x(i, &t) {
+            xvs.push((i, (start, end)));
+        }
+    }
+
+    let mut yvs = vec![];
+    for i in t.y0..=yv_max {
+        if let Some((start, end)) = hit_steps_y(i, &t) {
+            yvs.push((i, (start, end)));
+        }
+    }
+    let mut num = 0;
+    for xv in &xvs {
+        for yv in &yvs {
+            let max_start = max(xv.1 .0, yv.1 .0);
+            let min_end = min(xv.1 .1, yv.1 .1);
+            if max_start <= min_end {
+                num += 1;
+            }
+        }
+    }
+    num
 }
 
 #[cfg(test)]
 mod test {
 
     const TESTCASE: &str = "\
-";
+target area: x=20..30, y=-10..-5";
 
     #[test]
     fn test1() {
         let res = super::solve_part_1(TESTCASE);
-        assert_eq!(res, 0);
+        assert_eq!(res, 45);
     }
 
     #[test]
     fn test2() {
         let res = super::solve_part_2(TESTCASE);
-        assert_eq!(res, 0);
+        assert_eq!(res, 112);
     }
 }
