5be2052b9fb8e9e88879126ded8862f9dd7ba720
Fredrik Bergenlid <fredrik.bergenlid@gmail.com>
2021-12-27 11:05:07 +0100

    day 19

--- 2021/src/day20.rs
+++ 2021/src/day20.rs
@@ -1,4 +1,35 @@
-pub fn solve_part_1(_input_str: &str) -> usize {
+use hashbrown::HashMap;
+
+type EnhAlgo = Vec<u8>;
+type Image = HashMap<(i32, i32), u8>;
+
+fn parse(input: &str) -> (EnhAlgo, Image) {
+    let (iea, image_str) = input.split_once("\n\n").unwrap();
+    let algo: EnhAlgo = iea
+        .chars()
+        .map(|c| match c {
+            '#' => 1,
+            '.' => 0,
+            _ => panic!(""),
+        })
+        .collect();
+
+        let img =
+            image_str.lines().enumerate().map(
+    |row, s| {
+    s.chars().enumerate().map(|col, c| 
+        match c {
+            '#' => ((row, col), 1),
+            '.' => ((row, col), 0),
+            _ => panic!("");
+        }
+    ).collect()
+    });
+}
+
+pub fn solve_part_1(input_str: &str) -> usize {
+    let (iea, imagage) = parse(input_str);
+
     0
 }
 pub fn solve_part_2(_input_str: &str) -> usize {
@@ -9,12 +40,18 @@ pub fn solve_part_2(_input_str: &str) -> usize {
 mod test {
 
     const TESTCASE: &str = "\
-";
+..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+#..#.
+#....
+##..#
+..#..
+..###";
 
     #[test]
     fn test1() {
         let res = super::solve_part_1(TESTCASE);
-        assert_eq!(res, 0);
+        assert_eq!(res, 35);
     }
 
     #[test]
