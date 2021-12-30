60a57c260e6f2a371d3982876657e52fbf040c89
Fredrik Bergenlid <fredrik.bergenlid@gmail.com>
2021-12-30 22:54:00 +0100

    format

--- 2021/src/day16.rs
+++ 2021/src/day16.rs
@@ -108,10 +108,10 @@ mod test {
 
     #[test]
     fn test1() {
-        // assert_eq!(super::solve_part_1("8A004A801A8002F478"), 16);
-        // assert_eq!(super::solve_part_1("620080001611562C8802118E34"), 12);
-        // assert_eq!(super::solve_part_1("C0015000016115A2E0802F182340"), 23);
-        // assert_eq!(super::solve_part_1("A0016C880162017C3686B18A3D4780"), 31);
+        assert_eq!(super::solve_part_1("8A004A801A8002F478"), 16);
+        assert_eq!(super::solve_part_1("620080001611562C8802118E34"), 12);
+        assert_eq!(super::solve_part_1("C0015000016115A2E0802F182340"), 23);
+        assert_eq!(super::solve_part_1("A0016C880162017C3686B18A3D4780"), 31);
     }
 
     #[test]
